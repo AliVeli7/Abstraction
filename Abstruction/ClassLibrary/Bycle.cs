c8eee166b0af5156c43f6029714bc81418634de3
AliVeli7 <6gmrj9e@code.edu.az>
2022-03-31 08:34:11 +0400

    Created Hometask File

---
+++ Abstruction/ClassLibrary/Bycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Bycle:Vehicle
+    {
+        public override void Drive(double drivekm)
+        {
+            Millage += drivekm;
+            Console.WriteLine($"Millage: {Millage}");
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+    }
+}
---
+++ Abstruction/ClassLibrary/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public abstract class Vehicle
+    {
+        public string Color;
+        public string Brand;
+        public double Millage;
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} - Color: {Color} - Millage: {Millage} ");
+        }
+
+        public abstract void Drive(double drive);
+    }
+}
